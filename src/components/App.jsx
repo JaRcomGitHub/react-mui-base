9c084f6aa1b61a5d0dcfca8a8bfbcab1c02d6219
JaRcomGitHub <jarcom@ukr.net>
2022-12-05 21:44:13 +0200

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>React mui base</h1>
+      <br />
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
