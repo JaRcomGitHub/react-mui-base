bc8611af3c64edce55080f8a6183a5337c45c5f0
JaRcomGitHub <jarcom@ukr.net>
2022-12-07 18:54:16 +0200

    add new elem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup, Checkbox, Fab } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import AlarmIcon from '@mui/icons-material/Alarm';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export const App = () => {
   return (
@@ -59,6 +65,36 @@ export const App = () => {
       <IconButton aria-label="delete" size="large">
         <DeleteIcon fontSize="inherit" />
       </IconButton>
+      <br />
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+      <br />
+      <Checkbox {...label} defaultChecked />
+      <Checkbox {...label} />
+      <Checkbox {...label} disabled />
+      <Checkbox {...label} disabled checked />
+      <br />
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+      <Fab disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <br />
       
     </div>
